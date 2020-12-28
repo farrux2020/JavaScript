7a0d15198cb13606aa590b9edcfc68bd7b27c534
farrux2020 <75608888+farrux2020@users.noreply.github.com>
2020-12-28 15:08:19 +0500

    28.12.2020

---
+++ script.js
@@ -0,0 +1,31 @@
+var a = prompt ("Ismingizni kiriting");
+if (a == a){
+    alert("Xush kelibsiz " + a)
+}
+console.log("Mening ismim " +  a);
+
+
+var a = prompt( a + " Yoshingizni kiriting");
+console.log("Mening yoshim " + a + " da");
+
+var a = prompt("1-misol  10*5=?")
+console.log("1-misol 10*5=50  Sizning javobingiz " +  a);
+if (a == a) { alert ("1-misol 10*5=50  Sizning javobingiz " +  a)}
+
+var a = prompt("2-misol  20*2=?")
+console.log("2-misol 20*2=40  Sizning javobingiz " +  a);
+if (a == a) { alert ("2-misol 20*2=40  Sizning javobingiz " +  a)}
+
+var a = prompt("3-misol  10+6-8=?")
+console.log("3-misol 10+6-8=8  Sizning javobingiz " +  a);
+if (a == a) { alert ("3-misol 10+6-8=8  Sizning javobingiz " +  a)}
+
+var a = prompt("4-misol  95*5+25=?")
+console.log("4-misol 95*5+25=500  Sizning javobingiz " +  a);
+if (a == a) { alert ("4-misol 95*5+25=500  Sizning javobingiz " +  a)}
+
+var a = prompt("5-misol  25*6+8/2=?")
+console.log("5-misol 25*6+8/2=154  Sizning javobingiz " +  a);
+if (a == a) { alert ("5-misol 25*6+8/2=154  Sizning javobingiz " +  a)}
+
+if (a == a) { alert ("E'tiboringiz uchun Raxmat")}
